127ffbfef21c63d0e85c3946ca8515b5c9f40000
DmitriyBragin <36327306+DmitriyBragin@users.noreply.github.com>
2018-03-23 09:31:35 +0400

    Added usable Menu

--- src/MainWindow/mainwindow.cpp
+++ src/MainWindow/mainwindow.cpp
@@ -6,12 +6,28 @@
 #include <QProcessEnvironment>
 #include "src/Browser/browser.h"
 #include "src/Method/method.h"
+#include <QMenuBar>
+#include <QAction>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QMenu *file = menuBar()->addMenu("Меню"); //Создаем меню бар и помещаяем в него первый пункт меню file
+    QMenu *edit = menuBar()->addMenu("Настройки"); // Добавляем в наш меню бар дополнительный пункт edit
+    QAction *openFile = new QAction(tr("Открыть файл"), this); // Создаем экшн для нашего меню файл с пунктом new, так-же обязательно в качестве родителя оператором this указываем наше главное окно
+    openFile->setShortcut(tr("Ctrl+O")); // Создаем горячую клавишу для нашего экшена New
+    QAction *closeApp = new QAction(tr("Выход из программы"), this); // Создаем экшн для нашего меню файл с пунктом open, так-же обязательно в качестве родителя оператором this указываем наше главное окно
+    QAction *aboutApp = new QAction(tr("О программе"),this);
+
+    connect(openFile, SIGNAL(triggered()), this, SLOT(on_pushButton_2_clicked()));
+    connect(closeApp, SIGNAL(triggered()), this, SLOT(close()));
+    connect(aboutApp, SIGNAL(triggered()), this, SLOT(action()));
+
+    file->addAction(openFile); // Привязка экшена к меню File
+    file->addAction(closeApp); // Привязка экшена к меню File
+    edit->addAction(aboutApp); // Привязка экшена к меню edit
 }
 
 MainWindow::~MainWindow()
--- src/MainWindow/mainwindow.h
+++ src/MainWindow/mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QString>
+#include <QMessageBox>
 namespace Ui {
 class MainWindow;
 }
@@ -19,7 +20,12 @@ private slots:
     void on_pushButton_2_clicked();
     void on_pushButton_clicked();
     void on_textEdit_textChanged();
-
+    void action()
+    {
+        QMessageBox msg;
+        msg.setText("Action");
+        msg.exec();
+    }
 private:
     Ui::MainWindow *ui;
 };
