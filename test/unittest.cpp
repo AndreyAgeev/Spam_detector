19ff2b830eb8880d955171602b14b9fd60a124b0
AndreyAgeev <andreyageev1@mail.ru>
2018-03-26 16:42:49 +0300

    Update unit-test file A designer for the test class was added in unittest.h. A test call has been added to the functions main.cpp

--- test/unittest.cpp
+++ test/unittest.cpp
@@ -1,20 +1,13 @@
 #include "unittest.h"
-//#include "unittest.moc"
+
 void UnitTest::checkFile()
 {
-    QString str = "Hello";
-       QCOMPARE(str.toUpper(), QString("HELLO"));
 }
 void UnitTest::checkString()
 {
-    QString str = "Hello";
-       QCOMPARE(str.toUpper(), QString("HELLO"));
 }
 void UnitTest::checkResult()
 {
-    QString str = "Hello";
-       QCOMPARE(str.toUpper(), QString("HELLO"));
 }
 
-//QTEST_MAIN(UnitTest)
 
--- test/unittest.h
+++ test/unittest.h
@@ -1,14 +1,18 @@
 #ifndef UNITTEST_H
 #define UNITTEST_H
 #include <QtTest/QTest>
-#include <QObject>
+#include <QtCore/QObject>
+#include "src/MainWindow/mainwindow.h"
+
 class UnitTest : public QObject
 {
     Q_OBJECT
+public:
+    explicit UnitTest(QObject *parent = 0): QObject(parent){}
     private slots:
-        void checkFile();
-        void checkString();
-        void checkResult();
+    void checkFile();
+    void checkString();
+    void checkResult();
 };
 
 #endif // UNITTEST_H
