71f81f7e6dd151057146c86c1fc8fed161b601ab
AndreyAgeev <andreyageev1@mail.ru>
2018-04-25 13:59:58 +0300

    Added doxygen comments Comments have been added for two files: mainwindow.h, method.h

--- src/MainWindow/mainwindow.h
+++ src/MainWindow/mainwindow.h
@@ -38,15 +38,73 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    /**
+     * @brief Сообщение.
+     *
+     * Сообщения, для которого будет определяться его принадлежность к спаму
+     *
+     */
     QString mainString;
+    /**
+     * @brief Класс Method.
+     *
+     * Класс, содержащий методы для алгоритмов, определяющих спамю
+     *
+     */
     Method M; /* Main calculations method */
+    /**
+     * @brief Класс TextClass.
+     *
+     * Класс, предназначеный для вспромогательной обработки введенных сообщений
+     *
+     * @see Table
+     */
     TextClass txtCls; /* Class for normalize string */
+    /**
+     * @brief Класс Table.
+     *
+     * Класс, содержащий таблицу для обучения модели, на основании которой будет
+     * определяться принадлежность к спаму
+     *
+     * @see Method
+     */
     Table T; /* Table for learning */
 
 private slots:
+    /**
+     * @brief Открытие файлы и обучение модели.
+     *
+     * Сигнал, предназначеный для открытия файлы, предназначеного для обучения модели
+     * и запуск обучения
+     * @return @ref void
+     * @see  on_pushButton_2_clicked
+     */
     void on_pushButton_2_clicked(); /* open browser and teach main mechanism */
+    /**
+     * @brief Вывод результата работы.
+     *
+     * Сигнал, предназначеный для вывода результата обработки сообщения.
+     *
+     * @return @ref void
+     * @see  on_pushButton_2_clicked
+     */
     void on_pushButton_clicked(); /* Show results */
+    /**
+     * @brief Ввод данных.
+     *
+     * Сигнал, предназначеный для ввода сообщения.
+     *
+     * @return @ref void
+     * @see  on_pushButton_2_clicked
+     */
     void on_textEdit_textChanged(); /* catch string from input stream */
+    /**
+     * @brief Вывод информации о приложении.
+     *
+     * Сигнал, предназначеный для вывода окна, содержащего информацию о приложении
+     *
+     * @return @ref void
+     */
     void aboutApp(); /* 'About' message box */
 
 private:
