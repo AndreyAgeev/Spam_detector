5d788a1308d34c19d76c2f848df5d1935c4b3fb5
AndreyAgeev <36598717+AndreyAgeev@users.noreply.github.com>
2018-05-16 16:13:24 +0300

    Changed the path to the file "table.h"

--- src/Method/method.h
+++ src/Method/method.h
@@ -2,8 +2,7 @@
 #define METHOD_H
 #include <QString>
 #include <iostream>
-//#include "src/Table/table.h"
-#include "C:/Programming/project/src/Table/table.h"
+#include "./src/Table/table.h"
 class Method
 {
     public:
